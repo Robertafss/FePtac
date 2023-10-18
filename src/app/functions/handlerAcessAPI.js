f73ef3d0ea0e62bbf1ac9d4727b514d18987327e
robertafss <roberta.silva4@estudante.ifms.edu.br>
2023-10-18 18:31:00 -0400

    import

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,39 +1,29 @@
 'use server'
-const Users = [{
-    nome: 'Roberta',
-    email:'Roberta@gmail.com',
-    senha:'1212',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},{
-    nome: 'Thalia',
-    email:'Thalia@gmail.com',
-    senha:'1313',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},{
-    nome: 'Lucas',
-    email:'Lucas@gmail.com',
-    senha:'1414',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-},{
-    nome: 'Henrique',
-    email:'Henrique@gmail.com',
-    senha:'1515',
-    token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
+import { headers } from "next/dist/client/components/headers";
+
+const url = "https://aula-17-10-sand.vercel.app";
+const getUserAuthenticated = async (user) => {
+const responseOfApi = await fetch (url + "/user/authenticated",
+{
+    method: "POST", 
+    headers:{"content-type":"application/json"},
+    body: JSON.stringify(user)
 }
-
-];
-const getUserAuthenticated = async (userLogin) => {
-let userAuth = {};
-
-Users.map((user) => {
-    if (user.email === userLogin.email & user.senha === userLogin.password){
-        userAuth = user
-    }
-})
+);
+const userAuth = await responseOfApi.json();
 return userAuth;
+
+
+
 }
 
 const getUsers = () =>{
-        return Users;
+
+    const responseOfApi  = await fetch (url + "/users",
+    
+    const lista = await responseOfApi.json();
+
+    );
 }
 export { getUsers, getUserAuthenticated };
